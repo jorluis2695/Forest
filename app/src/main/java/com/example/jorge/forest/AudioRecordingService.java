9a0dbaf9ceef6dcf2e9a4ca959d88d95c763ad22
Jorge Cedeño <jlcedeno@espol.edu.ec>
2018-04-25 11:21:25 -0500

    Esqueleto de la aplicación

---
+++ app/src/main/java/com/example/jorge/forest/AudioRecordingService.java
@@ -0,0 +1,73 @@
+package com.example.jorge.forest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.media.MediaRecorder;
+
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class AudioRecordingService extends IntentService {
+//    // TODO: Rename actions, choose action names that describe tasks that this
+//    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+//    public static final String ACTION_FOO = "com.example.jorge.forest.action.FOO";
+//    public static final String ACTION_BAZ = "com.example.jorge.forest.action.BAZ";
+//
+//    // TODO: Rename parameters
+//    public static final String EXTRA_PARAM1 = "com.example.jorge.forest.extra.PARAM1";
+//    public static final String EXTRA_PARAM2 = "com.example.jorge.forest.extra.PARAM2";
+
+    private MediaRecorder mRecorder = null;
+
+
+
+
+    public AudioRecordingService() {
+        super("AudioRecordingService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+//        if (intent != null) {
+//            final String action = intent.getAction();
+//            if (ACTION_FOO.equals(action)) {
+//                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+//                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+//                handleActionFoo(param1, param2);
+//            } else if (ACTION_BAZ.equals(action)) {
+//                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+//                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+//                handleActionBaz(param1, param2);
+//            }
+//        }
+
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    //    /**
+//     * Handle action Foo in the provided background thread with the provided
+//     * parameters.
+//     */
+//    private void handleActionFoo(String param1, String param2) {
+//        // TODO: Handle action Foo
+//        throw new UnsupportedOperationException("Not yet implemented");
+//    }
+//
+//    /**
+//     * Handle action Baz in the provided background thread with the provided
+//     * parameters.
+//     */
+//    private void handleActionBaz(String param1, String param2) {
+//        // TODO: Handle action Baz
+//        throw new UnsupportedOperationException("Not yet implemented");
+//    }
+}
--- app/src/main/java/com/example/jorge/forest/MainActivity.java
+++ app/src/main/java/com/example/jorge/forest/MainActivity.java
@@ -4,28 +4,54 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.support.annotation.NonNull;
 
 import java.util.prefs.Preferences;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String mFileName = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
     }
+
+
 
 
     public void btnSettings_onClick(View view) {
